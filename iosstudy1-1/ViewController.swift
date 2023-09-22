43a11ff3d8491c4081ca5541a542390b3be695e5
s01ver <solverglace@gmail.com>
2023-09-22 15:07:03 +0900

    Initial Commit

---
+++ iosstudy1-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosstudy1-1
+//
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleImageView.layer.cornerRadius = 20
+    }
+
+    @IBOutlet var titleImageView: UIImageView!
+    
+}
+
